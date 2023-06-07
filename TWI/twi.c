ea97dd1189fd8056e792b7178495d195ca45cfe6
Edward Kisiel <hominoid@cablemi.com>
2023-06-07 18:14:20 -0400

    added dual mode, changed to using defined names and other initialization configuration

--- TWI/twi.c
+++ TWI/twi.c
@@ -5,7 +5,7 @@
  * Author:  Edward Kisiel
  * License: GNU GPLv3
  *
- * Description: AVRDx TWI driver to control the processor's I2C interface.
+ * Description: AVRDx TWI driver for I2C interface.
  *
 */
  
@@ -19,25 +19,35 @@
  *  twi/i2c initialization for all ports and modes.
  *   
  *  uint8_t twi_init(uint8_t TWI_MODE, uint8_t TWIPORT, uint8_t BAUD)
- *                   TWI_MODE = TWI_MODE_HOST, TWI_MODE_CLIENT
+ *                   TWI_MODE = TWI_MODE_HOST, TWI_MODE_CLIENT, TWI_MODE_DUAL
  *                    TWIPORT = TWI0, TWI1
- *                       BAUD = bus baud rate
+ *                       BAUD = bus baud rate 100000, 400000, 1000000
  */
-uint8_t twi_init(uint8_t TWI_MODE, uint8_t TWIPORT, uint8_t BAUD) {
+uint8_t twi_init(uint8_t TWI_MODE, uint8_t TWIPORT, uint32_t BAUD) {
 
     if(TWI_MODE == TWI_MODE_HOST) {
         switch(TWIPORT) {
             case TWI0_I2C :
-                // if used set SDAHOLD, FMPEN bits in TWI0_MCTRLA here
+
+                // configure INPUTLVL I2C, SDAHOLD 50ns, SDASETUP 4 cycles, TIMEOUT 100us
+                TWI0_CTRLA = TWI_INPUTLVL_I2C_gc || TWI_SDAHOLD_50NS_gc || TWI_SDASETUP_4CYC_gc || TWI_TIMEOUT_100US_gc;            
+                
+                if(BAUD == 1000000UL) {
+                    TWI0_CTRLA = TWI_FMPEN_ON_gc;
+                }
+                else {
+                    TWI0_CTRLA = TWI_FMPEN_OFF_gc;
+                }
 
                 // set baud rate
-                TWI0_MBAUD = BAUD;
+                TWI0_MBAUD = TWI0_BAUDRATE(BAUD, 0);
 
                 // set ENABLE bit
-                TWI0_MCTRLA &= 0b00000001;
+                TWI0_MCTRLA = TWI_ENABLE_bm;
 
-                // set BUSSTATE to force idle
-                TWI0_MSTATUS = 0x1;
+                // clear MSTATUS and set BUSSTATE to force idle
+                TWI0_MSTATUS = TWI_RIF_bm | TWI_WIF_bm | TWI_CLKHOLD_bm | TWI_RXACK_bm |
+                    TWI_ARBLOST_bm | TWI_BUSERR_bm | TWI_BUSSTATE_IDLE_gc;
                 
                 // check for idle state
                 if(TWI0_MSTATUS > 0) {
@@ -47,16 +57,25 @@ uint8_t twi_init(uint8_t TWI_MODE, uint8_t TWIPORT, uint8_t BAUD) {
                     return(1);
                 }
             case TWI1_I2C :
-                // if used set SDAHOLD, FMPEN bits in TWI1_MCTRLA here
+                // configure INPUTLVL I2C, SDAHOLD 50ns, SDASETUP 4 cycles, TIMEOUT 100us
+                TWI1_CTRLA =  TWI_INPUTLVL_I2C_gc || TWI_SDAHOLD_50NS_gc || TWI_SDASETUP_4CYC_gc || TWI_TIMEOUT_100US_gc;
+                
+                if(BAUD == 1000000UL) {
+                    TWI1_CTRLA = TWI_FMPEN_ON_gc;
+                }
+                else {
+                    TWI1_CTRLA = TWI_FMPEN_OFF_gc;
+                }
 
                 // set baud rate
-                TWI1_MBAUD = BAUD;
+                TWI1_MBAUD = TWI1_BAUDRATE(BAUD, 0);
 
                 // set ENABLE bit
-                TWI1_MCTRLA &= 0b00000001;
-
-                // set BUSSTATE to force idle
-                TWI1_MSTATUS = 0x1;
+                TWI1_MCTRLA = TWI_ENABLE_bm;
+                
+                // clear MSTATUS and set BUSSTATE to force idle
+                TWI1_MSTATUS = TWI_RIF_bm | TWI_WIF_bm | TWI_CLKHOLD_bm | TWI_RXACK_bm |
+                    TWI_ARBLOST_bm | TWI_BUSERR_bm | TWI_BUSSTATE_IDLE_gc;
                 
                 // check for idle state
                 if(TWI1_MSTATUS > 0) {
@@ -72,45 +91,68 @@ uint8_t twi_init(uint8_t TWI_MODE, uint8_t TWIPORT, uint8_t BAUD) {
     if(TWI_MODE == TWI_MODE_CLIENT) {
         switch(TWIPORT) {
             case TWI0_I2C :
-                // configure SDASETUP
-                TWI0_SCTRLA &= 0b00001000;
+                // configure INPUTLVL I2C, SDAHOLD 50ns, SDASETUP 4 cycles, TIMEOUT 100us
+                TWI0_CTRLA = TWI_INPUTLVL_I2C_gc || TWI_SDAHOLD_50NS_gc || TWI_SDASETUP_4CYC_gc || TWI_TIMEOUT_100US_gc;
 
                 // set address
                 TWI0_SADDR = TWI0_ADDR;
 
                 // set ENABLE bit
-                TWI0_SCTRLA &= 0b00000001;
+                TWI0_SCTRLA = TWI_ENABLE_bm;
                 
-                // set BUSSTATE to force idle
-                TWI0_SSTATUS = 0x1;
+                return(0);
                 
-                // check for idle state
-                if(TWI0_SSTATUS > 0) {
-                    return(0);
-                }
-                else {
-                    return(1);
-                }
             case TWI1_I2C :
-                 // configure SDASETUP
-                TWI1_CTRLA &= 0b00001000;
+                // configure INPUTLVL I2C, SDAHOLD 50ns, SDASETUP 4 cycles, TIMEOUT 100us
+                TWI1_CTRLA = TWI_INPUTLVL_I2C_gc || TWI_SDAHOLD_50NS_gc || TWI_SDASETUP_4CYC_gc || TWI_TIMEOUT_100US_gc;
+                
+                // configure SDASETUP to 4 cycles
+                TWI1_CTRLA = TWI_SDASETUP_4CYC_gc;
 
                 // set address
                 TWI1_SADDR = TWI1_ADDR;
 
                 // set ENABLE bit
-                TWI1_SCTRLA &= 0b00000001;
+                TWI1_SCTRLA = TWI_ENABLE_bm;
                                 
-                // set BUSSTATE to force idle
-                TWI1_SSTATUS = 0x1;
+                return(0);
+              
+            default :
+                return(1);
+        }
+    }
+    if(TWI_MODE == TWI_MODE_DUAL) {
+        switch(TWIPORT) {
+            case TWI0_I2C :
+                // configure SDAHOLD to 50ns
+                TWI0_CTRLA = TWI_SDAHOLD_50NS_gc;
                 
-                // check for idle state
-                if(TWI1_SSTATUS > 0) {
-                    return(0);
-                }
-                else {
-                    return(1);
-                }
+                // configure INPUTLVL I2C, SDASETUP 4 cycles
+                TWI0_DUALCTRL = TWI_INPUTLVL_I2C_gc || TWI_SDASETUP_4CYC_gc;
+
+                // set address
+                TWI0_SADDR = TWI0_ADDR;
+
+                // set ENABLE bit
+                TWI0_DUALCTRL = TWI_ENABLE_bm;
+                
+                return(0);
+                
+            case TWI1_I2C :
+                // configure SDAHOLD to 50ns
+                TWI1_CTRLA = TWI_SDAHOLD_50NS_gc;
+                
+                // configure INPUTLVL I2C, SDASETUP 4 cycles
+                TWI1_DUALCTRL = TWI_INPUTLVL_I2C_gc || TWI_SDASETUP_4CYC_gc;
+
+                // set address
+                TWI1_SADDR = TWI1_ADDR;
+
+                // set ENABLE bit
+                TWI1_DUALCTRL = TWI_ENABLE_bm;
+                                
+                return(0);
+              
             default :
                 return(1);
         }
--- TWI/twi.h
+++ TWI/twi.h
@@ -5,28 +5,35 @@
  * Author:  Edward Kisiel
  * License: GNU GPLv3
  *
- * Description: AVRDx TWI driver to control the processor's I2C interface.
+ * Description: AVRDx TWI driver for I2C interface.
  *
 */
+
+#define F_CPU                           24000000UL
+#define CLK_PER                         24000000UL
+
  
 /* TWI0 setttings */
-#define TWI0_I2C            0
-#define TWI0_ADDR           0x00
-#define TWI0_BAUD           128
+#define TWI0_I2C                        0
+#define TWI0_ADDR                       0x00
+#define TWI0_BAUD                       100000
+#define TWI0_BAUDRATE(F_SCL, T_RISE)    ((((((float)CLK_PER / (float)F_SCL)) - 10 - ((float)CLK_PER * T_RISE))) / 2)
 
 /* TWI1 setttings */
-#define TWI1_I2C            1
-#define TWI1_ADDR           0x69
-#define TWI1_BAUD           128
+#define TWI1_I2C                        1
+#define TWI1_ADDR                       0x69
+#define TWI1_BAUD                       100000
+#define TWI1_BAUDRATE(F_SCL, T_RISE)    ((((((float)CLK_PER / (float)F_SCL)) - 10 - ((float)CLK_PER * T_RISE))) / 2)
 
-#define TWI_MODE_HOST       0
-#define TWI_MODE_CLIENT     1
-#define TWI_READ_ACK        1
-#define TWI_READ_NACK       0
+#define TWI_MODE_HOST                   0
+#define TWI_MODE_CLIENT                 1
+#define TWI_MODE_DUAL                   1
+#define TWI_READ_ACK                    1
+#define TWI_READ_NACK                   0
 
 /* error messages */
-#define TWI_ERROR_NOHOST    10
+#define TWI_ERROR_NOHOST                10
 
-uint8_t twi_init(uint8_t TWI_MODE, uint8_t TWIPORT, uint8_t BAUD);
+uint8_t twi_init(uint8_t TWI_MODE, uint8_t TWIPORT, uint32_t BAUD);
 
 void error(uint8_t num);
